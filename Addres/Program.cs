bf606a6b769080100424fea5340fbe24ae2e577b
iterin <88264634+iterin@users.noreply.github.com>
2022-07-01 21:21:17 +0500

    Добавьте файлы проекта.

---
+++ Addres/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addres
+{
+    class Address
+    {
+        public string Street, Number;
+        public Address()
+        {
+            Street = "Lenina";
+            Number = "1a";
+        }
+
+    }
+    class House
+    {
+        public Address myAddress = new Address();
+        public int FloorCount = 0,
+            Floor = 0, 
+            FlatCount = 0, 
+            Flat = 0; 
+        public void Build()
+        {
+            Console.WriteLine("Дом построен");
+        }
+        public void Destroy()
+        {
+            Console.WriteLine("Дом снесён");
+        }
+    }
+    class Flat
+    {
+        public int Floor = 0,
+            NimberInHouse = 0,
+            NumberOfRooms = 0;
+        public decimal Square = 0;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
